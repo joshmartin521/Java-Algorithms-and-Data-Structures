d5ffd9c4972ee9a5cce40f92a6b773e752b056d7
Josh Martin <159808243+joshmartin521@users.noreply.github.com>
2024-06-11 09:33:56 +0100

    Create quickSort.java

---
+++ quickSort.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+public class quick {
+  public static void main(String[]args) {
+  	int[] array = {6,7,2,3,1,5,8,10,9};
+    quickSort(array, 0, array.length - 1);
+    printArray(array);
+  }
+
+  public static void quickSort(int arr[], int begin, int end) {
+      if (begin < end) {
+          int partitionIndex = partition(arr, begin, end);
+  
+          quickSort(arr, begin, partitionIndex-1);
+          quickSort(arr, partitionIndex+1, end);
+      }
+  }
+  
+  private int partition(int arr[], int begin, int end) {
+      int pivot = arr[end];
+      int i = (begin-1);
+  
+      for (int j = begin; j < end; j++) {
+          if (arr[j] <= pivot) {
+              i++;
+  
+              int swapTemp = arr[i];
+              arr[i] = arr[j];
+              arr[j] = swapTemp;
+          }
+      }
+  
+      int swapTemp = arr[i+1];
+      arr[i+1] = arr[end];
+      arr[end] = swapTemp;
+  
+      return i+1;
+  }
+
+  public static void printArray(int arr[]) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+}
