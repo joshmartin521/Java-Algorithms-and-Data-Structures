236ae691b0f75ea5616843965f00b81323f90143
Josh Martin <159808243+joshmartin521@users.noreply.github.com>
2024-08-02 09:16:40 +0100

    Create FlattenBinaryTree.java
    Flatten binary tree into a linked list in pre order

---
+++ FlattenBinaryTree.java
@@ -0,0 +1,41 @@
+class Solution {
+    // Function to flatten a binary tree
+    // to a right next Linked List structure
+    public void flatten(TreeNode root) {
+        // Initialize a pointer
+        // 'curr' to the root of the tree
+        TreeNode curr = root;
+
+        // Iterate until 'curr'
+        // becomes NULL
+        while (curr != null) {
+            // Check if the current
+            // node has a left child
+            if (curr.left != null) {
+                // If yes, find the rightmost
+                // node in the left subtree
+                TreeNode pre = curr.left;
+                while (pre.right != null) {
+                    pre = pre.right;
+                }
+
+                // Connect the rightmost node in
+                // the left subtree to the current
+                // node's right child
+                pre.right = curr.right;
+
+                // Move the entire left subtree to the
+                // right child of the current node
+                curr.right = curr.left;
+
+                // Set the left child of
+                // the current node to NULL
+                curr.left = null;
+            }
+
+            // Move to the next node
+            // on the right side
+            curr = curr.right;
+        }
+    }
+}
